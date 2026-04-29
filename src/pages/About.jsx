c5941733da2f32c5bd4309507681626cde037dee
ilyas-220 <ilyassamsudin388@gmail.com>
2026-04-29 17:32:19 +1200

    ''

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,11 @@ import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import img from '../img/1.png'
 import Floor from '../components/FLoor';
 import '../components/scene.css'
+import './about.css'
 
 export default function About() {
   return (
-    <div id='about' className=" pt-10 bg-black/50 text-gray-100 ">
+    <div id='about' className="back  pt-10 bg-black/50 text-gray-100">
       <div className="w-[80%] mx-auto pt-10 text-center">
         <h1 className="text-teal-500">ABOUT</h1>
         <h1 className="text-4xl font-bold">About Me</h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,37 @@
-import { FaCode, FaCss3, FaHtml5, FaJs, FaNodeJs, FaReact } from "react-icons/fa";
+import {
+  FaCode,
+  FaCss3,
+  FaHtml5,
+  FaJs,
+  FaNodeJs,
+  FaReact,
+} from "react-icons/fa";
 import img from "../img/1.png";
 import { BiSend } from "react-icons/bi";
-import { SiNestjs, SiTailwindcss,SiTypescript,SiVite,SiVitess,SiVitest } from "react-icons/si";
+import {
+  SiNestjs,
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+  SiVitess,
+  SiVitest,
+} from "react-icons/si";
 import Typing from "../data/Typed";
 import { RiNextjsFill } from "react-icons/ri";
 import { Send } from "lucide-react";
 import { VscVscode } from "react-icons/vsc";
+import "./home.css";
 
 export default function Home() {
   return (
-    <section id="home" className=" scroll-mt-5 min-h-screen  bg-black/50 pt-20 text-white">
+    <section
+      id="home"
+      className=" scroll-mt-5 min-h-screen  bg-black/50 pt-20 text-white">
       <div className="w-[90%] mx-[auto] grid gap-5 lg:grid md:grid-cols-2 lg:grid-cols-3 gap-10 min-h-[500px] relative">
-        <div className="hidden  absolute bg-gradient-to-br from-teal-50 to-green-100 dark:bg-gradient-to-br dark:from-teal-900 dark:to-blue-900 blur-3xl p-60  z-9 left-0 top-0 h-full"></div>
+        {/* left */}
         <div className="mih-h-[500px] px-2 flex flex-col gap-4 justify-center z-10 ">
           <div className=" ">
-            <h1 className="text-6xl  font-light text-gray-200">
-              I am
-            </h1>
+            <h1 className="text-6xl  font-light text-gray-200">I am</h1>
             <h1 className="text-6xl lg:text-8xl font-bold  dark:text-gray-100 ">
               Ilyas samsudin
             </h1>
@@ -31,15 +46,37 @@ export default function Home() {
             interfaces.
           </p>
           <div className="mt-2 flex gap-2">
-            <a href="#contact" className="px-5 py-2 rounded-2xl bg-teal-600 text-white shadow-lg">
+            <a
+              href="#contact"
+              className="px-5 py-2 rounded-2xl bg-teal-600 text-white shadow-lg">
               Contact Me
             </a>
-            <a href="#project" className="px-5 py-2 rounded-2xl border border-teal-500 text-teal-500 shadow-lg">
+            <a
+              href="#project"
+              className="px-5 py-2 rounded-2xl border border-teal-500 text-teal-500 shadow-lg">
               View Project
             </a>
           </div>
         </div>
-        <div
+
+        {/* center */}
+        <div className="boxFoto h-full rounded-3xl  ">
+          <img src={img} alt="" className="h-full w-full object-cover" />
+          {/* vs-code */}
+          <div className="bg-slate-900 shadow-lg rounded-lg p-2 absolute bottom-20 right-[-3%]">
+            <VscVscode className="text-5xl text-blue-500" />
+          </div>
+          {/* vite */}
+          <div className="bg-slate-900 shadow-lg p-2 absolute bottom-20 left-[-3%] rounded-lg ">
+            <SiVitess className="text-5xl text-purple-600" />
+          </div>
+          {/* email */}
+          <div className="p-3 -translate-y-5 bg-gray-800 w-full absolute bottom-[-5%] z-10 rounded-full flex justify-center gap-10 items-center text-teal-500">
+            <Send /> <span className="text-white">Ilyassamsudin@gmail.com</span>
+          </div>
+        </div>
+
+        {/* <div
           className=" rounded-3xl relative mx-2"
           data-aos="fade-up">
             <div className="bg-gradient-to-b from-teal-500 to-blue-500  h-[500px] rounded-full blur-3xl z-[9] ">
@@ -47,19 +84,18 @@ export default function Home() {
             <div className="bg-slate-900 shadow-lg rounded-lg p-2 absolute bottom-20 right-[-3%]">
               <VscVscode className="text-5xl text-blue-500"/>
             </div>
-            <div className="bg-slate-900 shadow-lg p-2 absolute bottom-20 left-[-3%] rounded-lg "><SiVitess className="text-5xl text-purple-600"/></div>
+           
           <img src={img} alt="" className=" absolute h-full object-cover  w-full top-0 z-[10] left-[-1%]" />
-          <div className="p-3 -translate-y-5 bg-gray-800 w-full absolute z-10 rounded-full flex justify-center gap-10 items-center text-teal-500">
-            <Send/> <span className="text-white">Ilyassamsudin@gmail.com</span>
-          </div>
-        </div>
+          
+        </div> */}
+
+        {/* right */}
+
         <div
-          className="p-5 mt-20 md:p-10 bg-gradient-to-b from-slate-800 to-black rounded-3xl"
+          className="p-5 mt-20 md:p-10 bg-gradient-to-b from-slate-800 to-black rounded-3xl  z-10"
           data-aos="fade-up"
           data-aos-delay="200">
-          <h1 className="text-6xl font-bold text-gray-200">
-            My Skills
-          </h1>
+          <h1 className="text-6xl font-bold text-gray-200">My Skills</h1>
           <div className="grid grid-cols-2 md:grid-cols-2 gap-2 mt-10">
             <div className="p-2 rounded-2xl  bg-slate-800 flex items-center gap-2">
               <FaHtml5 size={40} color="orange" />
@@ -78,7 +114,10 @@ export default function Home() {
               <h1>React</h1>
             </div>
             <div className="p-2 rounded-2xl bg-slate-800 flex items-center gap-2">
-              <SiTypescript size={40} className="text-blue-500 bg-white rounded-lg" />
+              <SiTypescript
+                size={40}
+                className="text-blue-500 bg-white rounded-lg"
+              />
               <h1>TypeScript</h1>
             </div>
             <div className="p-2 rounded-2xl bg-slate-800 flex items-center gap-2">
